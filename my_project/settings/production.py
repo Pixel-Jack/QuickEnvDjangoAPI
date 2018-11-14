ba96df42244aae354917cfb322d873a67c3ec001
ponthieu <clement.ponthieu@supelec.fr>
2018-11-14 01:42:01 +0100

    Add redis container

--- my_project/settings/production.py
+++ my_project/settings/production.py
@@ -27,3 +27,16 @@ DATABASES = {
         'TIMEZONE': 'UTC'
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
